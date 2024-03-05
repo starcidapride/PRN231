24fb082dfda4f618903600d5a1eb76ca83f9be84
Nguyen Tien Son <97070879+MarkSon2022@users.noreply.github.com>
2024-03-05 12:54:29 +0700

    .
    .

--- Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
+++ Service/obj/Debug/net8.0/Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86d08b35378412f41fba8a6fa4a64a38dd7d34b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc9f882ece40b6a8e02551e18cfd8b770f298950")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
